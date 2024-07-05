534da014a5eedba7bcb2704ab9c980805f3f4152
David Sonnet <120939807+partal251P@users.noreply.github.com>
2024-07-05 22:38:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from pytube import YouTube
-import os
-from moviepy.editor import AudioFileClip, VideoFileClip
+from pytube import YouTube, Playlist
+
 
 class Downloader():
-    def __init__(self):
-        self.list_res = ["144p", "240p", "360p", "480p", "720p", "1080p", "1440p", "2160p"]
-
     def fusion(self, list):
+        """
+        A simple Mergesort
+        :param list: list()
+        :return: list()
+        """
         if len(list) > 1:
             left = list[:len(list) // 2]
             right = list[len(list) // 2:]
@@ -36,23 +37,23 @@ class Downloader():
     def searching_in_streams(self, response, format):
         """
         Will take a list with all the objects and will sort them
-        :param response:
-        :param format:
-        :return:
+        :param response: str()
+        :param format: str()
+        :return: list(), list()
         """
         temp_list = {}
-        if format == "mp4":
+        if format == "2":
             stream = YouTube(response).streams.filter(file_extension="mp4")
-        elif format == "mp3":
+        elif format == "1":
             stream = YouTube(response).streams.filter(only_audio=True)
 
-        for element in stream:  #Creation of the dict with the quality : id
-            if format == "mp3" and element.abr:
+        for element in stream:  # Creation of the dict with the quality : id
+            if format == "1" and element.abr:
                 temp_list[element.abr] = element.itag
-            elif format == "mp4" and element.resolution:
+            elif format == "2" and element.resolution:
                 temp_list[element.resolution] = element.itag
 
-        if format == "mp3":  # Will use the function fusion to sort the different ints (quality)
+        if format == "1":  # Will use the function fusion to sort the different ints (quality)
             a = [i[:-4] for i in temp_list.keys()]
             sorted_audio = [i + "kbps" for i in self.fusion(a)]
             return temp_list, sorted_audio
@@ -62,52 +63,62 @@ class Downloader():
             return temp_list, sorted_audio
 
     def quality(self, response, format):
-        if format == "mp4":
+        if format == "2":
             sorted_resolutions = self.searching_in_streams(response, format)
             return sorted_resolutions
 
-        elif format == "mp3":
+        elif format == "1":
             sorted_audio = self.searching_in_streams(response, format)
             return sorted_audio
 
+    def download(self, link, format, id, destination):
+        """
+        Downloads
+        :param link: str
+        :param id: int
+        :return: None
+        """
 
+        if format == "2":
+            stream_video = YouTube(link).streams.get_by_itag(id)
+            out_file = stream_video.download(output_path=destination)
+        else:
+            stream_audio = YouTube(link).streams.get_by_itag(id).download(destination)
 
-    def download(self, link, id):
-        stream_video = YouTube(link).streams.get_by_itag(id)
-        stream_audio = YouTube(link).streams.filter(mime_type="audio/webm", only_audio=True).first()
-
-        destination = input("Enter the destination (leave blank for current directory): ")
-        out_file = stream_video.download(output_path=destination)
-        base, ext = os.path.splitext(out_file)
-        new_file = 'video' + '.mp4'
-        os.rename(out_file, new_file)
-
-        out_file = stream_audio.download(output_path=destination)
-        new_file = "audio" + '.webm'
-        os.rename(out_file, new_file)
-
-        input_video = VideoFileClip("video.mp4")
-        input_audio = AudioFileClip("audio.webm")
-
-        final = input_video.set_audio(input_audio).write_videofile(base+".mp4")
-        os.remove("video.mp4")
-        os.remove("audio.webm")
+    def playlist(self, link,format):
+        destination = input("Enter the destination (leave blank for current directory):\n>>")
+        playl = Playlist(link)
+        for url, vid in zip(playl.video_urls, playl.videos):
+            print(vid.streams.first().itag)
+            self.download(url, format, vid.streams.first().itag, destination)
 
     def main(self):
-        link = input("Hi, this uses pytube to download youtube videos. Enter the video link: ")
-        format = input("Enter the format you want to download (mp4/mp3): ")
+        choice = input("Hi, this uses pytube to download youtube videos. Do you want to download:"
+                       "(1) Playlist "
+                       "(2) Single video\n>>")
+        if choice == "1":
+            link = input("Enter the playlist link:\n>>")
+            print(Playlist("https://www.youtube.com/watch?v=FQxSwhee8KM&list=PLMyw82_22zVxokspEoncAuvOgp-SxnyjT"))
+            format = input("Enter the format you want to download: \n(1) Mp3\n(2) Mp4\n>>")
+            self.playlist(link, format)
 
-        while not len(link) in [90, 43, 99] or format not in ["mp4", "mp3"]:
-            link = input("Error, you entered a invalid link or format. Enter the link: ")
-            format = input("Enter the format you want to download (mp4/mp3): ")
-        option_quality = self.quality(link, format)
+        else:
+            link = input("Enter the video link:\n>>")
+            format = input("Enter the format you want to download: \n(1) Mp3\n(2) Mp4\n>>")
 
-        quality_choosen = input(str(option_quality[1]) + "   Choose the quality for the video/audio (ex: 48kbps/1080p)\n")
-        while quality_choosen not in option_quality[1]:
+            while not len(link) in [90, 43, 99, 95] or format not in ["1", "2"]:
+                link = input("Error, you entered a invalid link or format. Enter the link:\n>>")
+                format = input("Enter the format you want to download: \n(1) Mp3\n(2) Mp4\n>>")
 
-            if len(quality_choosen) == 6 and quality_choosen[2:] != "kbps" or len(quality_choosen) == 7 and quality_choosen[3:] != "kbps":
-                quality_choosen = input("The input is not in the list of the available quality. Try again (ex: 48kbps/1080p)\n")
-        self.download(link, option_quality[0][quality_choosen])
+            option_quality = self.quality(link, format)
+            quality_choosen = input(str(option_quality[1]) + " Choose the quality for the video/audio (ex: 48kbps/1080p)\n>>")
+            while quality_choosen not in option_quality[1]:  # Vérifie
+
+                if len(quality_choosen) == 6 and quality_choosen[2:] != "kbps" or len(quality_choosen) == 7 and quality_choosen[3:] != "kbps":
+                    quality_choosen = input("The input is not in the list of the available quality. Try again (ex: 48kbps/1080p)\n>>")
+            destination = input("Enter the destination (leave blank for current directory):\n>>")
+            self.download(link, format, option_quality[0][quality_choosen], destination)
+
 
 
 down = Downloader().main()
